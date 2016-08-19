59131ee70791e2a7542197f83316225f2052d2df
yuriel <yuriel3183@gmail.com>
2016-08-19 18:17:10 +0800

    add leak canary entry

--- components/src/main/kotlin/me/chunyu/yuriel/kotdebugtool/components/__TestingActivity.kt
+++ components/src/main/kotlin/me/chunyu/yuriel/kotdebugtool/components/__TestingActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import com.squareup.leakcanary.internal.DisplayLeakActivity
 import me.chunyu.yuriel.kotdebugtool.components.injector.__InjectorActivity
 
 /**
@@ -23,11 +24,18 @@ class __TestingActivity : Activity(), View.OnClickListener {
         result
     }
 
+    val button3 by lazy {
+        val result = findViewById(R.id.button3) as Button
+        result.setOnClickListener(this)
+        result
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.__activity_testing)
         button1
         button2
+        button3
     }
 
     override fun onClick(v: View) {
@@ -38,6 +46,10 @@ class __TestingActivity : Activity(), View.OnClickListener {
             R.id.button2 -> {
                 val intent = Intent(this, __InjectorActivity::class.java)
                 intent.putExtra(__InjectorActivity.TYPE, __InjectorActivity.TYPE_ALL_ACTIVITIES)
+                startActivity(intent)
+            }
+            R.id.button3 -> {
+                val intent = Intent(this, DisplayLeakActivity::class.java)
                 startActivity(intent)
             }
         }
