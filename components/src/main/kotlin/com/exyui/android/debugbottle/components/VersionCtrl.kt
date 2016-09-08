0312be3baf0668050e0b4c86ec695afdcce24c0d
yuriel <yuriel3183@gmail.com>
2016-09-08 12:57:52 +0800

    add v23 support

--- components/src/main/kotlin/com/exyui/android/debugbottle/components/VersionCtrl.kt
+++ components/src/main/kotlin/com/exyui/android/debugbottle/components/VersionCtrl.kt
@@ -1,11 +1,11 @@
 package com.exyui.android.debugbottle.components
 
 /**
- * Created by machine on 2016-09-07
+ * Created by machine on 2016-09-08
 
  * This file is generated automatically, DO NOT modify!!!
  */
 
 const val __DT_VERSION_CODE = 1
-const val __DT_VERSION_NAME = "1.0.0EAP-SNAPSHOT"
-const val __DT_BUILD_NUM = 160907
+const val __DT_VERSION_NAME = "1.0.0EAP-support23-SNAPSHOT"
+const val __DT_BUILD_NUM = 160908
