adbb9a5e6b24294035f7a52f1efe938e4e926efd
yuriel <yuriel3183@gmail.com>
2016-09-09 18:38:18 +0800

    add injector examples

--- demo/src/main/kotlin/me/chunyu/dev/yuriel/kotdebugtool/ContentInjector.kt
+++ demo/src/main/kotlin/me/chunyu/dev/yuriel/kotdebugtool/ContentInjector.kt
@@ -1,6 +1,7 @@
 package me.chunyu.dev.yuriel.kotdebugtool
 
 import android.content.Intent
+import android.os.Process
 import android.widget.Toast
 import com.exyui.android.debugbottle.components.injector.Injector
 
@@ -9,10 +10,20 @@ import com.exyui.android.debugbottle.components.injector.Injector
  */
 class ContentInjector: Injector() {
     override fun inject() {
-        put (Intent(activity, DemoActivity::class.java), "activity entry")
+        put (Intent(activity, DemoActivity::class.java), "Demo Activity entry example")
+
+        put (Intent(activity, ExampleActivity::class.java), "Setting Activity example")
 
         put (Runnable {
             Toast.makeText(activity, "Run!", Toast.LENGTH_SHORT).show()
-        }, "runnable entry")
+        }, "Toast a \"Run!\"")
+
+        put (Runnable {
+            Toast.makeText(activity, "Cache files delete!", Toast.LENGTH_SHORT).show()
+        }, "Delete cache files example")
+
+        put (Runnable {
+            Process.killProcess(Process.myPid())
+        }, "Kill process example")
     }
 }
