93a5298774fc643a382b84accc2de1c2690a811f
yuriel <yuriel3183@gmail.com>
2016-10-20 11:47:24 +0800

    change path

---
+++ noop-java/src/main/java/com/exyui/android/debugbottle/components/injectors/QuickEntry.java
@@ -0,0 +1,39 @@
+package com.exyui.android.debugbottle.components.injectors;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+
+import java.util.LinkedHashMap;
+
+/**
+ * Created by yuriel on 10/17/16.
+ */
+
+@SuppressWarnings("unused")
+public class QuickEntry {
+    private final String TAG = "QuickEntry";
+    public void put(Object a, Object b, Object c) {
+        Log.d(TAG, "put");
+    }
+    public void put(Object a, Object b) {
+        Log.d(TAG, "put");
+    }
+    public OnActivityDisplayedListener get() {
+        return null;
+    }
+    public void run(Object a) {
+        Log.d(TAG, "run");
+    }
+    public LinkedHashMap<String, OnActivityDisplayedListener> getList() {
+        return null;
+    }
+    public boolean isEmpty() {
+        return true;
+    }
+    interface OnActivityDisplayedListener {
+        boolean shouldShowEntry(Activity activity);
+        void run(Context context);
+        String description();
+    }
+}
